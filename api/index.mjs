8d78fa3b9c5c0824578ffa4cacca2e570e7c471d
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-04-06 04:33:07 +0100

    API: Versuch, SSL Bug zu beheben..

--- api/index.mjs
+++ api/index.mjs
@@ -7,8 +7,8 @@ app.use(cors({ credentials: true, origin: true }))
 app.options('*', cors())
 
 app.get('/', (request, result) => {
-  const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: true })
-  client.connect()
+  const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+  client.connect().catch(error => throw error)
   client.query('SELECT * FROM Lehrplan', (error, result) => {
     if (error) throw error
     result.json(result)
@@ -17,4 +17,4 @@ app.get('/', (request, result) => {
 })
 
 const port = process.env.PORT || 3000
-app.listen(port, (error) => console.log(error || `Server running on port ${port}.`))
+app.listen(port, (error) => { if (error) { throw error } else { console.log(`Server running on port ${port}.`}))
