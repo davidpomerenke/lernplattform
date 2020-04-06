931becfd74fda29ae0d7057cb6950a021c01e471
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-04-06 07:37:06 +0100

    API: Entferne CORS.

--- api/index.mjs
+++ api/index.mjs
@@ -1,10 +1,7 @@
 import express from 'express'
-import cors from 'cors'
 import pg from 'pg'
-const app = express()
 
-app.use(cors({ credentials: true, origin: true }))
-app.options('*', cors())
+const app = express()
 
 app.get('/', (request, result) => {
   let query = ''
